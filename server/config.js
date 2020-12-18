58f64f705c4f7f73ebdcc64986780d67c2f186f7
Erdem Uslu <usluerdem@yandex.com>
2020-12-18 10:23:26 +0300

    update :: port

--- server/config.js
+++ server/config.js
@@ -1,8 +1,14 @@
 const dotenv = require('dotenv');
+const { execSync } = require('child_process');
+const { userInfo } = require('os');
+
+const { username } = userInfo();
+const userIdCmd = execSync(`id -u ${username}`).toString();
+const userId = parseInt(userIdCmd, 10);
+const PORT = userId + 20000;
 
 const {
   NODE_ENV = 'development',
-  PORT = 8383,
 } = process.env;
 const env = dotenv.config().parsed;
 
